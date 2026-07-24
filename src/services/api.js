f9a21b533f36afdfa123e8b4ec1a47874f000a0f
khalsxaRa <studyweekluvchu@gmail.com>
2026-07-24 21:14:01 +0700

    evallagi

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://backendkp-production.up.railway.app/api",
+  baseURL: import.meta.env.VITE_API_URL || "https://backendkp-production-679e.up.railway.app",
 });
 
 api.interceptors.request.use((config) => {
