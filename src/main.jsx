5945050714dcb24a714ec2a30cc6a51e5a7adc37
Khalissa Raihanah Azhari <110162626+KhalsxaRa@users.noreply.github.com>
2026-07-25 17:17:28 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GoogleOAuthProvider clientId="704564637727-5p69rv9gt9qcpo6aqruuh9l25v5g82e9.apps.googleusercontent.com">
+    <GoogleOAuthProvider clientId=".apps.googleusercontent.com">
       <App />
     </GoogleOAuthProvider>
   </StrictMode>,
